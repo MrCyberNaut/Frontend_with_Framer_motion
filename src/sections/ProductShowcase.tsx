5914808c990022ad42414a118782b30b0a9aaf92
Arnav Sawant <arnavsawant.as@gmail.com>
2025-04-01 20:08:10 +0530

    product showcase motion

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,8 +1,18 @@
+"use client";
 import productImage from "@/assets/product-image.png";
 import Image from "next/image";
 import pyramindImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { use, useRef } from "react";
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+  // when the scroll progress is 0, the translateY is 0% and when the scroll progress is 1, the translateY is 100%
   return (
     <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
@@ -21,19 +31,25 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="Product Image" className="mt-10" />
-          <Image
-            src={pyramindImage}
+          <motion.img
+            src={pyramindImage.src}
             alt="Pyramid Image"
             width={262}
             height={262}
             className="hidden md:block absolute -right-36 -top-32"
+            style={{ translateY }}
+            // cuz translateY = transalteY so in JS we can skip writing the
+            // translateY: translateY
           />
-          <Image
-            src={tubeImage}
+          <motion.img
+            src={tubeImage.src}
             alt="Tube Image"
             width={248}
             height={248}
             className="hidden md:block absolute bottom-24 -left-36"
+            style={{ translateY }}
+            // cuz translateY = transalteY so in JS we can skip writing the
+            // translateY: translateY
           />
         </div>
       </div>
